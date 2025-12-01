d92c25b5bb90874b204bce85e57ce9090cf949ae
Adrian M. R. <adrianmridwan@gmail.com>
2025-12-01 14:31:34 +0700

    Add concurrentOrderedMap extensions (#20)
    * Add concurrentOrderedMap extensions
    
    * Remove unnecessary index function parameter

--- kt-coroutines-ext/build.gradle.kts
+++ kt-coroutines-ext/build.gradle.kts
@@ -1,5 +1,5 @@
 group = "com.drianmr.kotlin.ext"
-version = "1.0.1-SNAPSHOT"
+version = "1.0.5-SNAPSHOT"
 
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
--- kt-coroutines-ext/src/main/kotlin/com/drianmr/kotlin/ext/FlowExt.kt
+++ kt-coroutines-ext/src/main/kotlin/com/drianmr/kotlin/ext/FlowExt.kt
@@ -3,7 +3,10 @@ package com.drianmr.kotlin.ext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collectIndexed
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Transforms each element of the flow concurrently using the given [transform] function,
@@ -18,3 +21,37 @@ inline fun <T, R> Flow<T>.concurrentMapIndexed(
         }
     }
 }
+
+/**
+ * Executes a concurrent, indexed transformation on the elements of the [Flow] and
+ * returns the results as a [List], ensuring the final output order matches the
+ * original order of the input flow.
+ *
+ * @param transform A suspend lambda function that applies a transformation to each element.
+ */
+suspend inline fun <T, R> Flow<T>.concurrentOrderedMap(
+    crossinline transform: suspend (T) -> R,
+): List<R> = this
+    .concurrentMapIndexed { index, value -> index to transform(value) }
+    .toList()
+    .sortedBy { (index, _) -> index }
+    .map { (_, result) -> result }
+
+/**
+ * Executes a concurrent, indexed transformation on the elements of the [Flow] and
+ * returns the results as a [List], ensuring the final output order matches the
+ * original order of the input flow.
+ *
+ * @param flowOn The [CoroutineContext] (e.g., a specific Dispatcher) on which the upstream flow collection and
+ * concurrent mapping should be executed.
+ * @param transform A suspend lambda function that applies a transformation to each element.
+ */
+suspend inline fun <T, R> Flow<T>.concurrentOrderedMap(
+    flowOn: CoroutineContext,
+    crossinline transform: suspend (T) -> R,
+): List<R> = this
+    .concurrentMapIndexed { index, value -> index to transform(value) }
+    .flowOn(flowOn)
+    .toList()
+    .sortedBy { (index, _) -> index }
+    .map { (_, result) -> result }
