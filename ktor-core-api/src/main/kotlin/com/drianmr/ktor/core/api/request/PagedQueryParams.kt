ae36d38b2ebaacece22bcbda93812577fb062f25
Adrian M. R. <adrianmridwan@gmail.com>
2025-12-08 14:53:01 +0700

    Add safe parse PagedQueryParams using optionalFrom

--- ktor-core-api/src/main/kotlin/com/drianmr/ktor/core/api/request/PagedQueryParams.kt
+++ ktor-core-api/src/main/kotlin/com/drianmr/ktor/core/api/request/PagedQueryParams.kt
@@ -1,5 +1,7 @@
 package com.drianmr.ktor.core.api.request
 
+import com.drianmr.ktor.core.api.request.PagedQueryParams.Companion.DEFAULT_LIMIT
+import com.drianmr.ktor.core.api.request.PagedQueryParams.Companion.DEFAULT_PAGE
 import io.ktor.server.application.ApplicationCall
 
 data class PagedQueryParams(val page: Int, val limit: Int) {
@@ -9,14 +11,45 @@ data class PagedQueryParams(val page: Int, val limit: Int) {
         require(limit > 0) { "Limit must be greater than 0" }
     }
 
+    /**
+     * Calculates the starting offset based on the current [page] and [limit].
+     */
     val offset: Int = if (page > 1) (page - 1) * limit else 0
 
     companion object {
 
+        const val DEFAULT_PAGE = 1
+        const val DEFAULT_LIMIT = 10
+
+        /**
+         * Extracts and parses pagination parameters.
+         *
+         * Missing or invalid query values default to [DEFAULT_PAGE] and [DEFAULT_LIMIT].
+         */
         fun from(call: ApplicationCall): PagedQueryParams = call.request.queryParameters.let {
             PagedQueryParams(
-                page = it["page"]?.toIntOrNull() ?: 0,
-                limit = it["limit"]?.toIntOrNull() ?: 0,
+                page = it["page"]?.toIntOrNull() ?: DEFAULT_PAGE,
+                limit = it["limit"]?.toIntOrNull() ?: DEFAULT_LIMIT,
+            )
+        }
+
+        /**
+         * Conditionally extracts optional pagination parameters.
+         *
+         * Returns `null` if **both** `page` and `limit` are missing from the query. If one is present,
+         * any missing or invalid value defaults to [DEFAULT_PAGE] or [DEFAULT_LIMIT].
+         */
+        fun optionalFrom(call: ApplicationCall): PagedQueryParams? = call.request.queryParameters.let {
+            val page = it["page"]?.toIntOrNull()
+            val limit = it["limit"]?.toIntOrNull()
+
+            if (page == null && limit == null) {
+                return@let null
+            }
+
+            PagedQueryParams(
+                page = page ?: DEFAULT_PAGE,
+                limit = limit ?: DEFAULT_LIMIT,
             )
         }
     }
